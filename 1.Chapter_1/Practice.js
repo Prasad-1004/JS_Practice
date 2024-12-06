a1802a9ecce217cb46ca05514d207514c67a32d9
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-12-06 14:46:59 +0000

    js

---
+++ 1.Chapter_1/Practice.js
@@ -0,0 +1,18 @@
+//Create a variable of type string and try to add a number to it
+
+let a = "jit";
+let b = 10;
+console.log(a+b)
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(a+b));
+
+
+//Write a Js Program to create a word-meaning dictionary of 5 words
+let a1 = {
+    code : "jit",
+    code1 : "sunny",
+    code2 : "tukai"
+}
+
+console.log(a1);
---
+++ 2.Chapter_2/Practice.js
@@ -0,0 +1,12 @@
+//Use Logical Operator  to find whether the age of a person lies between 10 and 20
+
+//const prompt = require('prompt-sync')();
+//let a = prompt("Enter your age: ");
+
+let a = 15
+if(a>=10 && a<=20){
+    console.log("The age of person lies between 10 and 20");
+
+}else {
+    console.log("The age of person doesn't lie between 10 and 20");
+}
